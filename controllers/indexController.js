ba37733b87dd2b19b149b00e9179ee3a1475f415
VoidOfMaya <mayaofthevoid@gmail.com>
2026-01-20 08:44:17 +0200

    feat (views): create basic webpages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,15 @@
 async function getHome(req, res){
     res.render('index');
 }
+async function getCategories(req, res){
+    res.render('categories');
+}
+async function getItems(req, res){
+    res.render('items');
+}
 
 module.exports = {
     getHome,
+    getCategories,
+    getItems
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,12 @@
 //tools
 const {Router}= require('express');
 //controllers
-const {getHome} = require('../controllers/indexController.js')
+const {getHome, getCategories, getItems} = require('../controllers/indexController.js')
 
 const indexRouter =Router();
 //get
-indexRouter.get('/',getHome)
+indexRouter.get('/',getHome);
+indexRouter.get('/Category',getCategories);
+indexRouter.get('/Items',getItems);
 
 module.exports = indexRouter
